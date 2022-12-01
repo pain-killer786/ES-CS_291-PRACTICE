ae60c6edb7d4b06d804329f08de1912239ddf1be
Krishnendu Nandy <krishnendunandy43@gmail.com>
2022-12-01 15:49:49 +0530

    Create PRG3.C

---
+++ PRG3.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("ABC %d PQR",150);
+    printf("\n XYZ %d PQR %f", 12,78.85);
+    printf("\n PQR %d STC %c, 12+5,'P'");
+    printf("\n 1234 %d 567 %f 90",12,6.50);
+    return 0;
+}

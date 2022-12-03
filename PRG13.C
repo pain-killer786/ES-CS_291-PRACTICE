df6b9a85ee9c7cede624f52f3521630bdcca33d6
Krishnendu Nandy <krishnendunandy43@gmail.com>
2022-12-03 12:50:23 +0530

    Create PRG13.C
    Write a C program to print Pattern.

---
+++ PRG13.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  int n,i,j,k=0;
+  printf("\n Enter the values:");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++,k=0)
+  {
+    for(j=1;j<=n-i;j++)
+    {
+      printf(" ");
+    }
+    while(k!=2*i-1)
+    {
+      printf("0 ");
+      k++;
+    }
+    printf("\n");
+  }
+  return 0;
+}
